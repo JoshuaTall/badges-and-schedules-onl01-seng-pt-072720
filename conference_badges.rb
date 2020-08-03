d85fda74566f97be8e6b98bec31baef02d317d60
Joshua Tall <tall.joshua1984@yahoo.com>
2020-08-03 21:09:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-   = batch_badge_creator(attendees)
+  resultOne = batch_badge_creator(attendees)
   resultOne.each do |x|
     puts x 
   end
